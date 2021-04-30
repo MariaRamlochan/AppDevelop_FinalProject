20b7da314dcbf86b98855b56573ead46b2076834
Nafees-Syed <syednafees1997@gmail.com>
2021-04-30 12:56:15 -0400

    copy what I had

---
+++ ParkingGarageAutomation/ParkingGarageAutomation/Login.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace ParkingGarageAutomation
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.enterButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Neo Sans", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(201, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(273, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bear Park && associates";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Neo Sans", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(76, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Neo Sans", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(79, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 26);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password: ";
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.idTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.idTextBox.Location = new System.Drawing.Point(215, 144);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(259, 29);
+            this.idTextBox.TabIndex = 3;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.passwordTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordTextBox.Location = new System.Drawing.Point(215, 195);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(259, 29);
+            this.passwordTextBox.TabIndex = 4;
+            // 
+            // enterButton
+            // 
+            this.enterButton.Font = new System.Drawing.Font("Neo Sans", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enterButton.Location = new System.Drawing.Point(389, 250);
+            this.enterButton.Name = "enterButton";
+            this.enterButton.Size = new System.Drawing.Size(85, 33);
+            this.enterButton.TabIndex = 5;
+            this.enterButton.Text = "Enter";
+            this.enterButton.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label4.Location = new System.Drawing.Point(206, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(268, 10);
+            this.label4.TabIndex = 6;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(561, 314);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.enterButton);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.idTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button enterButton;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ ParkingGarageAutomation/ParkingGarageAutomation/Main.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace ParkingGarageAutomation
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fNameTextBox = new System.Windows.Forms.TextBox();
+            this.lNameTextBox = new System.Windows.Forms.TextBox();
+            this.checkInButton = new System.Windows.Forms.Button();
+            this.checkOutButton = new System.Windows.Forms.Button();
+            this.receiptLabel = new System.Windows.Forms.Label();
+            this.logoutButton = new System.Windows.Forms.Button();
+            this.parkingSpotLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Neo Sans", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(52, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Neo Sans", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(55, 200);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Last Name: ";
+            // 
+            // fNameTextBox
+            // 
+            this.fNameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fNameTextBox.Location = new System.Drawing.Point(202, 164);
+            this.fNameTextBox.Name = "fNameTextBox";
+            this.fNameTextBox.Size = new System.Drawing.Size(251, 22);
+            this.fNameTextBox.TabIndex = 2;
+            // 
+            // lNameTextBox
+            // 
+            this.lNameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lNameTextBox.Location = new System.Drawing.Point(202, 200);
+            this.lNameTextBox.Name = "lNameTextBox";
+            this.lNameTextBox.Size = new System.Drawing.Size(251, 22);
+            this.lNameTextBox.TabIndex = 3;
+            // 
+            // checkInButton
+            // 
+            this.checkInButton.Font = new System.Drawing.Font("Neo Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkInButton.Location = new System.Drawing.Point(359, 239);
+            this.checkInButton.Name = "checkInButton";
+            this.checkInButton.Size = new System.Drawing.Size(94, 34);
+            this.checkInButton.TabIndex = 4;
+            this.checkInButton.Text = "Check in";
+            this.checkInButton.UseVisualStyleBackColor = true;
+            // 
+            // checkOutButton
+            // 
+            this.checkOutButton.Font = new System.Drawing.Font("Neo Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkOutButton.Location = new System.Drawing.Point(359, 279);
+            this.checkOutButton.Name = "checkOutButton";
+            this.checkOutButton.Size = new System.Drawing.Size(94, 34);
+            this.checkOutButton.TabIndex = 5;
+            this.checkOutButton.Text = "Check out";
+            this.checkOutButton.UseVisualStyleBackColor = true;
+            // 
+            // receiptLabel
+            // 
+            this.receiptLabel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.receiptLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.receiptLabel.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.receiptLabel.Location = new System.Drawing.Point(476, 9);
+            this.receiptLabel.Name = "receiptLabel";
+            this.receiptLabel.Size = new System.Drawing.Size(204, 353);
+            this.receiptLabel.TabIndex = 6;
+            this.receiptLabel.Text = "label3";
+            // 
+            // logoutButton
+            // 
+            this.logoutButton.Font = new System.Drawing.Font("Neo Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logoutButton.Location = new System.Drawing.Point(598, 376);
+            this.logoutButton.Name = "logoutButton";
+            this.logoutButton.Size = new System.Drawing.Size(82, 30);
+            this.logoutButton.TabIndex = 7;
+            this.logoutButton.Text = "Logout";
+            this.logoutButton.UseVisualStyleBackColor = true;
+            // 
+            // parkingSpotLabel
+            // 
+            this.parkingSpotLabel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.parkingSpotLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.parkingSpotLabel.Location = new System.Drawing.Point(59, 249);
+            this.parkingSpotLabel.Name = "parkingSpotLabel";
+            this.parkingSpotLabel.Size = new System.Drawing.Size(157, 157);
+            this.parkingSpotLabel.TabIndex = 8;
+            this.parkingSpotLabel.Text = "label3";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 431);
+            this.Controls.Add(this.parkingSpotLabel);
+            this.Controls.Add(this.logoutButton);
+            this.Controls.Add(this.receiptLabel);
+            this.Controls.Add(this.checkOutButton);
+            this.Controls.Add(this.checkInButton);
+            this.Controls.Add(this.lNameTextBox);
+            this.Controls.Add(this.fNameTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Main";
+            this.Text = "Parking Automation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox fNameTextBox;
+        private System.Windows.Forms.TextBox lNameTextBox;
+        private System.Windows.Forms.Button checkInButton;
+        private System.Windows.Forms.Button checkOutButton;
+        private System.Windows.Forms.Label receiptLabel;
+        private System.Windows.Forms.Button logoutButton;
+        private System.Windows.Forms.Label parkingSpotLabel;
+    }
+}
+
